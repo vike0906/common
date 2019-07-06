39b96386af5b9446b464a8d1a9a059a7996a7329
vike0906 <liujialong688@outlook.com>
2019-07-06 18:48:49 +0800

    string()

--- src/common/RandomString.java
+++ src/common/RandomString.java
@@ -1,16 +1,41 @@
 package common;
 
+import java.security.SecureRandom;
+import java.util.Random;
 import java.util.UUID;
 
 public class RandomString {
 	
+	/**生成UUID*/
 	public static String uuid() {
 		String uuid = UUID.randomUUID().toString();
 		return uuid.replaceAll("-", "");
 	}
 	
+	/**生成长度为6的随机字符串*/
+	public static String string() {
+		return string(6);
+	}
+	
+	/**生成长度可定的随机字符串*/
+	public static String string(int length) {
+		String alphabets = "abcdefghijklmnopqrstuvwxyz0123456789";
+		Random random = new SecureRandom();
+		StringBuffer sb = new StringBuffer();
+		for(int i=0;i<length;i++) {
+			int index = random.nextInt(alphabets.length());
+			sb.append(alphabets.charAt(index));
+		}
+		return sb.toString();
+	}
+	
+	
+	
+	
 	public static void main(String [] args) {
 		System.out.println(RandomString.uuid());
+		System.out.println(RandomString.string(32));
+		System.out.println(RandomString.string());
 	}
 
 }
